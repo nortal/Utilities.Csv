8ce1175dc44c51b1dc0f96388ec3d98a33c3b1fe
Imre Pühvel <imre.pyhvel@gmail.com>
2016-12-18 21:45:21 +0200

    Version bump to v0.10.0

--- Csv/Properties/AssemblyInfo.cs
+++ Csv/Properties/AssemblyInfo.cs
@@ -20,12 +20,12 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("Nortal.Utilities.Csv")]
-[assembly: AssemblyDescription("")]
 [assembly: AssemblyProduct("Nortal.Utilities.Csv")]
-[assembly: AssemblyCopyright("Copyright © Imre Pühvel, Nortal AS 2013")]
+[assembly: AssemblyDescription("Tool for reading and writing CSV files. See more: https://github.com/NortalLTD/Utilities.Csv")]
+[assembly: AssemblyCopyright("Copyright © Imre Pühvel, Nortal AS 2013-2016")]
 
 [assembly: ComVisible(false)]
 
-[assembly: AssemblyVersion("0.9.2.0")]
+[assembly: AssemblyVersion("0.10.0.0")]
 
 [assembly: InternalsVisibleTo("Nortal.Utilities.Csv.Tests, PublicKey=002400000480000094000000060200000024000052534131000400000100010045e74d784f6844f0b36b4d340ebce42c4d35a639c6ad7eb56fe582db1ee17b3cce3b71165b2435a05d2703e61df1f3e2e7155dc811668ce58722005cba4d88af78060ff7d24dcff791a90684b7202ae90be8cbf695ea0fa0fec09027b7845282932dafe399f86a508dd356223247579293fdc9d69745c9454dac7ed6af7050b1")]
